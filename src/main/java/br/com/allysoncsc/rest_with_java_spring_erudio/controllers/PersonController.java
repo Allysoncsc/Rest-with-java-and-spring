8f1bd641d30be2c6944e6986b4d1fe70bf562a56
Allyson <allysoncsc@gmail.com>
2025-06-12 07:51:48 -0300

    Criação do endpoint finallperson

--- src/main/java/br/com/allysoncsc/rest_with_java_spring_erudio/controllers/PersonController.java
+++ src/main/java/br/com/allysoncsc/rest_with_java_spring_erudio/controllers/PersonController.java
@@ -5,10 +5,9 @@ import br.com.allysoncsc.rest_with_java_spring_erudio.model.Person;
 import br.com.allysoncsc.rest_with_java_spring_erudio.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/person")
@@ -20,6 +19,11 @@ public class PersonController {
     @RequestMapping(value = "/findbyid/{id}",method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable("id") String id){
         return personService.findById(id);
+    }
+
+    @GetMapping("/findall")
+    public List<Person> findAll(){
+        return personService.findAll();
     }
 
 }
--- src/main/java/br/com/allysoncsc/rest_with_java_spring_erudio/service/PersonService.java
+++ src/main/java/br/com/allysoncsc/rest_with_java_spring_erudio/service/PersonService.java
@@ -4,6 +4,8 @@ package br.com.allysoncsc.rest_with_java_spring_erudio.service;
 import br.com.allysoncsc.rest_with_java_spring_erudio.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -14,6 +16,15 @@ public class PersonService {
     private Logger logger = Logger.getLogger(PersonService.class.getName());
 
 
+    public List<Person> findAll(){
+        List<Person> persons =  new ArrayList<Person>();
+        for(int i = 0; i<5;i++){
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
     public Person findById(String id){
         logger.info("Finding one person!");
 
@@ -23,6 +34,17 @@ public class PersonService {
         person.setLastName("Correia");
         person.setAddress("Ceará - Brasil");
         person.setGender("Male");
+        return person;
+    }
+
+
+    public Person mockPerson(int i){
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("FirstName "+i);
+        person.setLastName("LastName "+i);
+        person.setAddress("Some place in Brasil");
+        person.setGender(i%2==0? "Male":"Female");
         return person;
     }
 
