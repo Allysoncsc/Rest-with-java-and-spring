0aa5378699f5b19a445731e435801a6828adcb00
Allyson <allysoncsc@gmail.com>
2025-06-12 11:05:40 -0300

    Adição do plugin jpa no maven, anotações na tabela person

--- src/main/java/br/com/allysoncsc/rest_with_java_spring_erudio/model/Person.java
+++ src/main/java/br/com/allysoncsc/rest_with_java_spring_erudio/model/Person.java
@@ -1,6 +1,7 @@
 package br.com.allysoncsc.rest_with_java_spring_erudio.model;
 
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,15 +10,23 @@ import lombok.Setter;
 import java.io.Serializable;
 
 
+@Entity
+@Table(name="person")
 public class Person implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name="first_name",nullable = false,length = 80)
     private String firstName;
+    @Column(name="last_name",nullable = false,length = 80)
     private String lastName;
+    @Column(nullable = false,length = 120)
     private String address;
+    @Column(nullable = false,length = 6)
     private String gender;
 
     public Person() {
